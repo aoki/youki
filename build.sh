a7f19a9906abd85cdf31edb5dc1e990e6ba390e4
aoki <ringohub@gmail.com>
2021-06-03 00:28:24 +0900

    Add cargo-when and target
    - Add cargo-when
    - Add target using `rustup`

--- build.sh
+++ build.sh
@@ -2,12 +2,14 @@
 
 TARGET=${TARGET-x86_64-unknown-linux-gnu}
 if [ "$TARGET" != "" ]; then
+    rustup target add ${TARGET}
     TGT="--target $TARGET"
 fi
 VERSION=debug
 if [[ "$1" == "--release" ]]; then
     VERSION=release
 fi
+cargo install cargo-when
 cargo when --channel=stable build --verbose $TGT $1 && \
 cargo when --channel=beta build --verbose $TGT $1 && \
 cargo when --channel=nightly build --verbose --features nightly $TGT $1 && \
